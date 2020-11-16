35c15e4802f353136199eea4a69d5dcfbee252c8
Alex Ferreira <alex.redux@gmail.com>
2020-11-16 11:08:39 +0000

    Drive.cs Update
    Now with Reverse Speed Action!

--- Assets/Drive.cs
+++ Assets/Drive.cs
@@ -8,6 +8,8 @@ public class Drive : MonoBehaviour
     public float m_Speed;
     public float turn_Speed;
 
+    public bool isReversed = false;
+
     void Start()
     {
         //Fetch the Rigidbody component you attach from your GameObject
@@ -18,36 +20,64 @@ public class Drive : MonoBehaviour
 
     void Update()
     {
+        isReversed = false;
+
         if (Input.GetKey(KeyCode.A))
         {
             MoveL();
         }
+
         if (Input.GetKey(KeyCode.D))
         {
             MoveR();
         }
+
+        switch (Input.touchCount)
+        {
+            case 1:
+                if (Input.touches[0].position.x < Screen.width / 2 || Input.GetKey(KeyCode.A))
+                {
+                    //Debug.Log("L");
+                    MoveL();
+                }
+
+                if (Input.touches[0].position.x > Screen.width / 2 || Input.GetKey(KeyCode.D))
+                {
+                    //Debug.Log("R");
+                    MoveR();
+                }
+                break;
+
+            case 2:
+                if (Input.touches[0].position.x < Screen.width / 2 && Input.touches[1].position.x > Screen.width / 2)
+                {
+                    isReversed = true;
+                }
+                break;
+        }
+
+        Movement();
+
+        /*
         if (Input.touchCount > 0)
         {
-        if (Input.touches[0].position.x < Screen.width / 2 || Input.GetKey(KeyCode.A))
-        {
-            //Debug.Log("L");
-            MoveL();
+            if (Input.touches[0].position.x < Screen.width / 2 || Input.GetKey(KeyCode.A))
+            {
+                //Debug.Log("L");
+                MoveL();
+            }
+
+            if (Input.touches[0].position.x > Screen.width / 2 || Input.GetKey(KeyCode.D))
+            {
+                //Debug.Log("R");
+                MoveR();
+            }
         }
-
-        if (Input.touches[0].position.x > Screen.width / 2 || Input.GetKey(KeyCode.D))
-        {
-            //Debug.Log("R");
-            MoveR();
-        }
-        }
-
-        //Move the Rigidbody forwards constantly at speed you define (the blue arrow axis in Scene view)
-        m_Rigidbody.velocity = transform.forward * m_Speed;
-
-
-
+        */
     }
+
     Vector3 RotationY = new Vector3(0, 0, 0);
+
     void MoveL()
     {
         //Debug.Log(transform.rotation.y);
@@ -61,5 +91,19 @@ public class Drive : MonoBehaviour
         //Debug.Log(transform.rotation.y);
         RotationY.y += turn_Speed;
         transform.rotation = Quaternion.Euler(RotationY);
+    }
+
+    void Movement()
+    {
+        if(isReversed)
+        {
+            m_Rigidbody.velocity = transform.forward * -m_Speed;
+        }
+
+        if(!isReversed)
+        {
+            //Move the Rigidbody forwards constantly at speed you define (the blue arrow axis in Scene view)
+            m_Rigidbody.velocity = transform.forward * m_Speed;
+        }
     }
 }
