e37376c1d72a5ad1875ff287675404eff3b71d87
Alex Ferreira <alex.redux@gmail.com>
2020-12-03 14:11:07 +0000

    ARTrackBuilder

---
+++ Assets/_Vitor/TrackBuilder.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackBuilder : MonoBehaviour
+{
+    public Object _prefab;
+
+    GameObject[] _objects;
+
+    TrackSlot[,] TrackGrid;
+
+    private void Awake()
+    {
+        _objects = new GameObject[400];
+
+        for(int i = 0; i < _objects.Length; i++)
+        {
+            _objects[i] = (GameObject)Instantiate(_prefab, transform);
+        }
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    void CreateGrid(int sizeX, int sizeY)
+    {
+        TrackGrid = new TrackSlot[sizeX, sizeY];
+
+        for (int x = 0; x < sizeX; x++)
+        {
+            int index = 0;
+
+            for (int y = 0; y < sizeY; y++)
+            {
+                index++;
+
+                TrackGrid[x, y] = _objects[index].GetComponent<TrackSlot>();
+            }
+        }
+    }
+}
---
+++ Assets/_Vitor/TrackSlot.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackSlot : MonoBehaviour
+{
+    enum State { EMPTY, READY, FULL }
+
+    State CurrentState = State.EMPTY;
+
+    bool hasPiece = false;
+
+    GameObject Piece;
+
+    TrackSlot()
+    {
+        hasPiece = false;
+    }
+
+    void ClickAction(/*outerPiece*/)
+    {
+        if(hasPiece)
+        {
+            //Ask to delete
+        }
+        else
+        {
+            //PlacePiece()
+        }
+
+    }
+
+    void PlacePiece()
+    {
+
+    }
+
+    void DeletePiece()
+    {
+        Destroy(Piece);
+        hasPiece = false;
+    }
+}
